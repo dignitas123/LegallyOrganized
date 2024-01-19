a23a19a057ceae601a00b5b5c290e00fdea25882
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2024-01-19 23:05:00 +0100

    fix: add allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-6qftytzc%)!9a@_6**(#7qwh95e40#v9%+rtfv*=72gue_wx0h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
